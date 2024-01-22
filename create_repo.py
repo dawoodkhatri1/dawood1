6646c235322aa07672745d4837c731d31f0607e8
dawoodkhatri1 <136968266+dawoodkhatri1@users.noreply.github.com>
2024-01-22 15:58:31 +0500

    Update create_repo.py

--- create_repo.py
+++ create_repo.py
@@ -20,6 +20,3 @@ if r.status_code == 201:  # Use r.status_code for comparison
 else:
     t = threading.Thread(target=print_message, args=("Error occurred! Repository not created",))
     t.start()
-
-#export PAT = "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"
-
